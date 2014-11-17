9dcfff9caec5cf5cea6193da404c8b11cec26797
Alex Engel <atengel@stanford.edu>
2014-11-16 18:39:01 -0800

    Added give and get food buttons. Give food brings you to a form.

--- app/src/main/java/com/cs147/flavr/createEvent.java
+++ app/src/main/java/com/cs147/flavr/createEvent.java
@@ -4,23 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
 
 
-public class MainActivity extends Activity {
+public class createEvent extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_event);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_create_event, menu);
         return true;
     }
 
@@ -37,9 +35,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void giveFood(View view) {
-        Intent goToForm = new Intent(this, createEvent.class);
-        startActivity(goToForm);
     }
 }
