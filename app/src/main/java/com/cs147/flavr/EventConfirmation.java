0ebc05e1eed054dc5ebb148c2d976624c2e0cb40
Alex Engel <atengel@stanford.edu>
2014-11-20 14:49:19 -0800

    Got address lookup working. Entire flow of give food now works. Error checking and polishing need to be done.

--- app/src/main/java/com/cs147/flavr/EventConfirmation.java
+++ app/src/main/java/com/cs147/flavr/EventConfirmation.java
@@ -1,206 +1,122 @@
-    //NOTICE: THIS CODE CONTAINS MATERIAL THAT IS FREELY DISTRIBUTED BY GOOGLE.INC
-    package com.cs147.flavr;
+//NOTICE: THIS CODE CONTAINS MATERIAL THAT IS FREELY DISTRIBUTED BY GOOGLE.INC
+package com.cs147.flavr;
 
 
 
-    import android.app.Activity;
-    import android.app.FragmentManager;
-    import android.content.IntentSender;
-    import android.location.Location;
-    import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.IntentSender;
+import android.location.Address;
+import android.location.Location;
+import android.os.Bundle;
+import java.sql.Time;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
-    import com.google.android.gms.common.GooglePlayServicesClient;
-    import com.google.android.gms.common.GooglePlayServicesUtil;
-    import com.google.android.gms.location.LocationClient;
-    import com.google.android.gms.maps.GoogleMap;
-    import com.google.android.gms.maps.MapFragment;
-    import android.widget.TextView;
-    import android.support.v4.app.FragmentActivity;
-    import android.view.Menu;
-    import android.view.MenuItem;
-    import android.content.Intent;
-    import android.view.View;
-    import android.app.FragmentTransaction;
-    import android.location.LocationManager;
-    import android.widget.Toast;
-    import com.google.android.gms.common.ConnectionResult;
-    import com.google.android.gms.maps.model.MarkerOptions;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import java.lang.String;
+import java.util.Calendar;
+import com.google.android.gms.maps.model.LatLng;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.location.Geocoder;
+import java.util.Locale;
+import android.location.Address;
+import java.util.List;
+import java.io.IOException;
 
-    import android.app.DialogFragment;
-    import android.app.Dialog;
-    import android.util.Log;
-    import android.content.Context;
-    import com.google.android.gms.maps.model.LatLng;
-
-    public class EventConfirmation extends FragmentActivity implements
-        GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener {
-
-    private final static int
-            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-    public LocationClient myLocationClient;
-        Location currentLocation;
-
-    public static class ErrorDialogFragment extends DialogFragment {
-        //Contains error dialog
-        private Dialog myDialog;
-
-        public ErrorDialogFragment() {
-            super();
-            myDialog = null;
+public class EventConfirmation extends FragmentActivity{
+    private TextView mAddress;
+    public LatLng getLocationFromAddress(String strAddress) {
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        LatLng loc = null;
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if(address == null) return null;
+            Address location = address.get(0);
+            loc = new LatLng(location.getLatitude(), location.getLongitude());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        public void setDialog(Dialog dialog) {
-            myDialog = dialog;
-        }
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return myDialog;
-        }
+        return loc;
     }
 
-    /*
-    * Handlepublic FragmentTransaction getSupportFragmentManager() {
-        return supportFragmentManager;
-    } results returned to the FragmentActivity
-    * by Google Play services
-    */
-    @Override
-    protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
-        // Decide what to do based on the original request code
-        switch (requestCode) {
-            case CONNECTION_FAILURE_RESOLUTION_REQUEST :
-    /*
-    * If the result code is Activity.RESULT_OK, try
-    */
-                switch (resultCode) {
-                    case Activity.RESULT_OK :
-    /*
-     * Try the request again
-     */
-                        break;
-                }
-        }
-    }
-    private boolean servicesConnected() {
-        // Check that Google Play services is available
-        int resultCode =
-                GooglePlayServicesUtil.
-                        isGooglePlayServicesAvailable(this);
-        // If Google Play services is available
-        if (ConnectionResult.SUCCESS == resultCode) {
-            // In debug mode, log the status
-            Log.d("Location Updates",
-                    "Google Play services is available.");
-            // Continue
-            return true;
-            // Google Play services was not available for some reason.
-            // resultCode holds the error code.
-        } else {
-            // Get the error dialog from Google Play services
-            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-                    resultCode,
-                    this,
-                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-            // If Google Play services can provide an error dialog
-            if (errorDialog != null) {
-                // Create a new DialogFragment for the error dialog
-                ErrorDialogFragment errorFragment =
-                        new ErrorDialogFragment();
-                // Set the dialog in the DialogFragment
-                errorFragment.setDialog(errorDialog);
-
-            }
-        }
-        return false;
-    }
-
-    @Override
-    //Location service calls this when successfully connected.
-    public void onConnected(Bundle bundle) {
-        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    //Location Service calls this if there is an error.
-    public void onDisconnected() {
-        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        if(connectionResult.hasResolution()) {
-            try{
-                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
-            }
-            catch (IntentSender.SendIntentException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void setLocation() {
-        currentLocation = myLocationClient.getLastLocation();
-        double latitude = currentLocation.getLatitude();
-        double longitude = currentLocation.getLongitude();
-        MapFragment temp = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        GoogleMap eventMap = temp.getMap();
-        LatLng location = new LatLng(latitude,longitude);
-        MarkerOptions newMarker = new MarkerOptions();
-        newMarker.position(location);
-        eventMap.addMarker(newMarker);
-
-    }
-
-
-//    protected void onStart() {
-//        super.onStart();
-//        // Connect the client.
-//        myLocationClient.connect();
-//    }
-//
-//    protected void onStop() {
-//        // Disconnecting the client invalidates it.
-//        myLocationClient.disconnect();
-//        super.onStop();
-//    }
-
-        public void printEventInfo(String [] information) {
-            TextView eventTitle = (TextView) findViewById(R.id.confirmation_food);
-            eventTitle.setText(information[0]);
-            TextView food = (TextView) findViewById(R.id.confirmation_event);
-            food.setText("Event: "+information[1]);
-            TextView description = (TextView) findViewById(R.id.confirmation_description);
-            description.setText("Description: "+information[2]);
-            TextView location = (TextView) findViewById(R.id.confirmation_location);
-            location.setText("Location: "+information[3]);
-            TextView tags = (TextView) findViewById(R.id.confirmation_tags);
-            tags.setText("Tags: "+information[4]);
-            TextView capacity = (TextView) findViewById(R.id.confirmation_capacity);
-            capacity.setText("Capacity: "+information[5]);
+    public void printEventInfo(String [] information) {
+        TextView eventTitle = (TextView) findViewById(R.id.confirmation_food);
+        eventTitle.setText(information[0]);
+        TextView food = (TextView) findViewById(R.id.confirmation_event);
+        food.setText("Event: "+information[1]);
+        TextView description = (TextView) findViewById(R.id.confirmation_description);
+        description.setText("Description: "+information[2]);
+        TextView location = (TextView) findViewById(R.id.confirmation_location);
+        location.setText("Location: "+information[3]);
+        TextView tags = (TextView) findViewById(R.id.confirmation_tags);
+        tags.setText("Tags: "+information[4]);
+        TextView capacity = (TextView) findViewById(R.id.confirmation_capacity);
+        capacity.setText("Capacity: "+information[5]);
 
 
 
 //            TextView keywords = (TextView) findViewById(R.id.keywords);
 //            keywords.setText("Keywords: "+information[4]);
-        }
-
-        protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        //myLocationClient = new LocationClient(this, this, this);
-        //setLocation();
-            Intent eventInfo = getIntent();
-            setContentView(R.layout.activity_event_confirmation);
-            String [] eventInformation = eventInfo.getStringArrayExtra(createEvent.TO_CREATE);
-            printEventInfo(eventInformation);
-
-    //       MapFragment temp = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-    //       GoogleMap eventMap = temp.getMap();
-    //       if(mapIsAvailable(eventMap)) {
-    //
-    //       }
     }
 
+    private void printEventExpiry(int[] times) {
+        Calendar c = Calendar.getInstance();
+        int sysHour = c.get(Calendar.HOUR_OF_DAY);
+        int sysMin = c.get(Calendar.MINUTE);
+        int startHour = times[0];
+        int startMin = times[1];
+        int endHour = times[2];
+        int endMin = times[3];
+        TextView startText = (TextView) findViewById(R.id.confirmation_start_time);
+        if (startHour > sysHour) {
+            int hourDiff = startHour - sysHour;
+            int minDiff = startMin - sysMin;
+            if(sysMin > startMin) hourDiff--;
+            startText.setText("Your event starts in " + Integer.toString(hourDiff) + "hours and "+ Integer.toString(minDiff)+"minutes.");
+        }
+        if(startHour == sysHour && startMin >= sysMin) {
+            int minDiff = startMin - sysMin;
+            startText.setText("Your event starts in "+ Integer.toString(minDiff)+" minutes.");
+        }
+        TextView endText = (TextView) findViewById(R.id.confirmation_end_time);
+        if (endHour > sysHour) {
+            int hourDiff = endHour - sysHour;
+            int minDiff = endMin - sysMin;
+            if(sysMin > endMin) hourDiff--;
+            endText.setText("Your event ends in " + Integer.toString(hourDiff) + "hours and "+ Integer.toString(minDiff)+"minutes.");
+        }
+        if(endHour == sysHour && endMin >= sysMin) {
+            int minDiff = endMin - sysMin;
+            endText.setText("Your event expires in "+ Integer.toString(minDiff)+" minutes.");
+        }
+
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent eventInfo = getIntent();
+        setContentView(R.layout.activity_event_confirmation);
+        String[] eventInformation = eventInfo.getStringArrayExtra(createEvent.EVENT_STRINGS);
+        LatLng location = getLocationFromAddress(eventInformation[3]);
+        GoogleMap gMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        gMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        int[] eventTimes = eventInfo.getIntArrayExtra(createEvent.EVENT_TIMES);
+        printEventInfo(eventInformation);
+        printEventExpiry(eventTimes);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -227,4 +143,4 @@
     //    public void printConfirmation(View view) {
     //
     //    }
-    }
+}
